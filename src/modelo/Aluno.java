cc5cd720e43ef681a572a03e3c63ee8dcc8f2282
Thais Salgado <salgadothais15@gmail.com>
2018-09-26 19:48:06 -0300

    adição do arquivo gitgnore e atuliazação da classe Aluno

--- src/modelo/Aluno.java
+++ src/modelo/Aluno.java
@@ -5,6 +5,16 @@ public class Aluno {
 	private int matricula;
 	private String nome;
 	private String cpf;
+	private String email;
+	
+	
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 	public int getMatricula() {
 		return matricula;
 	}
