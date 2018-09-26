00312c719b244f7e74fb7722f5dacc44d02fc7e3
Thais Salgado <salgadothais15@gmail.com>
2018-09-26 19:31:58 -0300

    Commit inicial

---
+++ src/modelo/Aluno.java
@@ -0,0 +1,28 @@
+package modelo;
+
+public class Aluno {
+
+	private int matricula;
+	private String nome;
+	private String cpf;
+	public int getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(int matricula) {
+		this.matricula = matricula;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	
+}
